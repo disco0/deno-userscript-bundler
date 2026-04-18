7f5091390aac219edc4ed58465a09b80cec4968b
__ <disco0@users.noreply.github.com>
2026-04-18 02:48:18 -0700

    [deps] update @deno/emit and add @std/jsonc

--- deps.ts
+++ deps.ts
@@ -8,6 +8,8 @@ export {readLines} from 'https://deno.land/std@0.217.0/io/mod.ts';
 export * as path from 'https://deno.land/std@0.217.0/path/mod.ts';
 export type { Reader } from "https://deno.land/std@0.217.0/io/types.ts";
 
-export * as emit from "https://deno.land/x/emit@0.22.0/mod.ts";
+export * as emit from "jsr:@deno/emit";
+// Should move to https://jsr.io/@std/jsonc
 export * as jsonc from "https://deno.land/x/jsonc@1/main.ts";
+export * as stdJsonc from 'jsr:@std/jsonc'
 export { default as os } from "https://deno.land/x/dos@v0.11.0/mod.ts";
